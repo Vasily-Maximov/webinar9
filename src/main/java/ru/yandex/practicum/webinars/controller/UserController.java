69636d0b1921314a1036eeecc6de4f512b6edc32
Vasily Maximov <softservice-31@mail.ru>
2023-03-21 19:42:18 +0300

    add user validation for registration

--- src/main/java/ru/yandex/practicum/webinars/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/webinars/controller/UserController.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.webinars.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.webinars.model.User;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -13,10 +16,16 @@ public class UserController {
     private int idGenerator = 1;
 
     @PostMapping()
-    public User register(@RequestBody User user) {
-        user.setId(idGenerator++);
-        users.add(user);
-        return user;
+    public User register(@RequestBody @Valid User user) {
+        if (users.stream().noneMatch(u ->u.getLogin().equals(user.getLogin()))) {
+            user.setId(idGenerator++);
+            users.add(user);
+            log.error("Пользователь с логином {} добавлен", user.getLogin());
+            return user;
+        } else {
+            log.error("Пользователь с логином {} уже существует", user.getLogin());
+            throw new RuntimeException("Пользователь с таким логином уже существует!");
+        }
     }
 
     @GetMapping
